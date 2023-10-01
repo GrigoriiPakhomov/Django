f039c0c43360755290936dd838d82eb12789f6bd
Grigorii Pakhomov <pakhomov-86@list.ru>
2023-10-01 15:23:53 +0800

    add converter

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, "year4")
+
 
 urlpatterns = [
-    path("", views.index), #http://127.0.0.1:8000
-    path("cats/", views.categories), #http://127.0.0.1:8000/cats
+    path("", views.index),  # http://127.0.0.1:8000
+    path("cats/<int:cat_id>/", views.categories),  # http://127.0.0.1:8000/cats/123/
+    path("cats/<slug:cat_slug>/", views.categories_by_slug),  # http://127.0.0.1:8000/cats/asdf/
+    path("archive/<year4:year>/", views.archive)
 ]
