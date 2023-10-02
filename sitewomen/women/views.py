20fb4e9c1e71a99219811d82428598f0b8e62a30
Grigorii Pakhomov <pakhomov-86@list.ru>
2023-10-02 21:26:08 +0800

    add redirect

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
 
 def index(request):
@@ -17,6 +17,9 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2023:
+        return redirect('home')
+
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 
