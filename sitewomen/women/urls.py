130ca6b18981c1093297f3a0b15d3513db639746
Grigorii Pakhomov <pakhomov-86@list.ru>
2023-10-09 18:26:29 +0800

    created a template directory and added 2 new templates

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -8,6 +8,7 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path("", views.index, name='home'),  # http://127.0.0.1:8000
+    path('about/', views.about, name='about'),
     path("cats/<int:cat_id>/", views.categories, name='cats_id'),  # http://127.0.0.1:8000/cats/123/
     path("cats/<slug:cat_slug>/", views.categories_by_slug, name='cats'),  # http://127.0.0.1:8000/cats/asdf/
     path("archive/<year4:year>/", views.archive, name='archive')
