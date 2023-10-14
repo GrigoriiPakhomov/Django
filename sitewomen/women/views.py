2adaad2ec68791a8e90acef3a9bd3c9a69c944dc
Grigorii Pakhomov <pakhomov-86@list.ru>
2023-10-14 13:54:00 +0800

    add new templates

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -26,24 +26,8 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте'})
+    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
-
-
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
-
-
-def archive(request, year):
-    if year > 2023:
-        return redirect('home')
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
