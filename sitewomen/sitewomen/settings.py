0946203a738cd4a64382f8099ecb8ccff7d29547
Grigorii Pakhomov <pakhomov-86@list.ru>
2023-10-17 16:47:09 +0800

    add static

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!&v71sdy!b4e(vpog9s%e+^mw2*f^x5!yv0xh8*bjaf@yn9ps*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+# STATICFILES_DIRS = [ BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
