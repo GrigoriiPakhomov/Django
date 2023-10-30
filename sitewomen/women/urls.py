414784ab41bcc80a4761263f0f0b1ecd146ef146
Grigorii Pakhomov <pakhomov-86@list.ru>
2023-10-30 21:39:08 +0800

    add slugs

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -9,7 +9,7 @@ register_converter(converters.FourDigitYearConverter, "year4")
 urlpatterns = [
     path("", views.index, name='home'),  # http://127.0.0.1:8000
     path('about/', views.about, name='about'),
-    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('post/<slug:post_slug>/', views.show_post, name='post'),
     path('addpage', views.addpage, name='addpage'),
     path('contact', views.contact, name='contact'),
     path('login', views.login, name='login'),
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template.loader import render_to_string
 
+from women.models import Women
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'addpage'},
@@ -22,10 +23,11 @@ cats_db = [
 ]
 
 def index(request):
+    posts = Women.objects.filter(is_published=1)
     data = {
         'title': 'главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
@@ -35,8 +37,17 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
