4da5737bc9dbe7f25dd89e55b513fc3d5d39d921
Grigorii Pakhomov <pakhomov-86@list.ru>
2024-01-30 10:33:51 +0800

    add pagination

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-import settings
+from django.conf import settings
 from women import views
 from women.views import page_not_found
 
